c1db735a73a3c680ed7482394b841b7ce057d389
Felipe <1felipefmachado@gmail.com>
2022-03-13 08:16:58 -0300

    initial application

---
+++ src/main/java/br/com/carteira/CarteiraAcoesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraAcoesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraAcoesApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/carteira/domain/model/Empresa.java
@@ -0,0 +1,5 @@
+package br.com.carteira.domain.model;
+
+public class Empresa {
+
+}
